c7062b5ddbdad9c0988809ce7437ea784147a6a7
Reductions <slavi.boyanov@gmail.com>
2016-08-22 21:52:50 +0300

    My emacs setup

---
+++ init.el
@@ -0,0 +1,151 @@
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" ."http://melpa.org/packages/") t)
+
+;; activate all the packages (in particular autoloads)
+(package-initialize)
+
+;; being behind bars isnt fun for me
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; disable blinking cursor
+(blink-cursor-mode -1)
+
+;; the only kind of bell that I would listen to is "The Devision Bell"
+(setq ring-bell-function 'ignore)
+
+;; disable startup screen
+(setq inhibit-startup-screen t)
+
+;; nice scrolling
+(setq scroll-margin 0
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
+
+;; mode line settings
+(line-number-mode t)
+(column-number-mode t)
+(size-indication-mode t)
+
+;; making up my face
+(set-face-attribute 'default nil :font "Consolas")
+(set-face-attribute 'default (selected-frame) :height 109)
+
+;; bind eshell
+(global-set-key (kbd "C-x e") 'eshell)
+
+;; split window in two
+(split-window-right)
+
+;; Prefer utf-8 encoding
+(prefer-coding-system 'utf-8)
+
+;; Do not use tabs for indentation
+(setq-default indent-tabs-mode nil)
+
+;; y/n instead of yes/no
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; show me the time
+(display-time-mode t)
+
+;; coursor you won't hide from me no more
+(set-cursor-color "red")
+(setq default-frame-alist
+      '((cursor-color . "red")))
+
+;; Highlight incremental search
+(setq search-highlight t)
+(transient-mark-mode t)
+
+;; Disable backups
+(setq backup-inhibited t)
+
+;; disable auto save
+(setq auto-save-default nil)
+
+;; some staff
+(global-set-key [f2] 'comment-region)
+(global-set-key [f3] 'uncomment-region)
+(global-set-key [f4] 'goto-line)
+(global-set-key [f5] 'indent-region)
+
+;; line numbers
+(add-hook 'prog-mode-hook 'linum-mode)
+
+;; white space display
+(add-hook 'prog-mode-hook 'whitespace-mode)
+
+;; javascript indent
+(setq js-indent-level 2)
+
+(require 'use-package)
+
+;; become evil
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1))
+
+(use-package solarized-theme
+  :ensure t
+  :config
+  (load-theme 'solarized-dark t))
+
+(use-package magit
+  :ensure t
+  :bind ("C-x g" . magit-status))
+
+(use-package evil-magit
+  :ensure t)
+
+(use-package ido
+  :ensure t
+  :config (ido-mode +1))
+
+(use-package auto-complete
+  :ensure t
+  :config
+  (ac-config-default))
+
+(use-package inf-ruby
+  :ensure t
+  :config
+  (add-hook 'ruby-mode-hook #'inf-ruby-minor-mode))
+
+(use-package smartparens
+  :ensure t
+  :config
+  (require 'smartparens-ruby)
+  (smartparens-global-mode)
+  (show-smartparens-global-mode t))
+
+(use-package evil-smartparens
+  :ensure t
+  :config
+  (add-hook 'smartparens-enabled-hook #'evil-smartparens-mode))
+
+(use-package projectile
+  :ensure t
+  :bind ("C-x p" . projectile-command-map)
+  :config
+  (projectile-global-mode +1))
+
+(use-package powerline-evil
+  :ensure t
+  :config
+  (powerline-evil-center-color-theme))
+
+(use-package fic-mode
+  :ensure t
+  :config (add-hook 'prog-mode-hook 'fic-mode))
+
+(use-package elm-mode
+  :ensure t
+  :config
+  (add-hook 'elm-mode-hook #'elm-oracle-setup-ac)
+  (setq elm-foramt-on-save t))
